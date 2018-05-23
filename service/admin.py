cf2d5b81365b708b82e55248d0467864ef64b113
Adonyaeva <jevis007@icloud.com>
2018-05-24 00:03:32 +0200

    Admin area was added

--- service/admin.py
+++ service/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Address
+from .models import TimeSlot
+from .models import Ticket
+from .models import Engineer
+from .models import Service
 
-# Register your models here.
+admin.site.register(Address)
+admin.site.register(TimeSlot)
+admin.site.register(Ticket)
+admin.site.register(Engineer)
+admin.site.register(Service)
+
