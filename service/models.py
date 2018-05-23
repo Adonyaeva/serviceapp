12a25e948bbedf74bd0df7409dfe98e342d6d320
Adonyaeva <jevis007@icloud.com>
2018-05-23 23:38:54 +0200

    Models were added

---
+++ service/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Address(models.Model):
+    street_name = models.CharField(max_length=200)
+    house_id = models.IntegerField()
+    house_number = models.IntegerField()
+    flat_number = models.IntegerField()
+    flat_id = models.IntegerField()
+
+
+    def __str__(self):
+        return self.street_name + self.house_number + self.flat_number
+
+
+class TimeSlot(models.Model):
+    from_begin = models.DateTimeField()
+    to = models.DateTimeField()
+    available = models.BooleanField()
+
+
+    def __str__(self):
+        return self.from_begin + self.to
+
+
+class Ticket(models.Model):
+    created_time = models.DateTimeField(default=timezone.now)
+    comment = models.TextField()
+    address = models.ForeignKey('service.Address', on_delete=models.CASCADE)
+    service = models.ForeignKey('service.Service', on_delete=models.CASCADE)
+    status_id = models.IntegerField()
+    status_updated_time = models.DateTimeField()
+    time_slot = models.ForeignKey('service.TimeSlot', on_delete=models.DO_NOTHING)
+    speciality = models.CharField(max_length=200)
+    engineer = models.ForeignKey('service.Engineer', on_delete=models.DO_NOTHING)
+    spent_time = models.IntegerField()
+
+    def __str__(self):
+        return self.address + self.service + self.time_slot
+
+
+class Engineer(models.Model):
+    name_full = models.CharField(max_length=400)
+    name_short = models.CharField(max_length=100)
+    speciality = models.CharField(max_length=200)
+    time_slots = models.ManyToManyField('service.TimeSlot')
+
+
+    def __str__(self):
+        return self.name_full
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=400)
+    description = models.TextField()
+    estimate = models.IntegerField()
+    type = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.name
+
